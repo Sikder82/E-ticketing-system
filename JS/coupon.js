60a689b13def0dcfb17c3f5dfd3ff787272f96dd
Md Mofijul Haq Sikder <mofijulhoq280382@gmail.com>
2024-06-10 18:47:26 +0600

    Web programming project submit

---
+++ JS/coupon.js
@@ -0,0 +1,71 @@
+const usedCoupn = document.getElementById('usedCoupn');
+const applyButton = document.getElementById('applyButton');
+const firstCoupn = document.getElementById('firstCoupn').innerText;
+    const secondCoupn = document.getElementById('secondCoupn').innerText;
+
+ function unhideArea(){
+    const hiddenArea = document.getElementById('hiddenArea');
+    const coupnArea = document.getElementById('coupnArea');
+    hiddenArea.classList.remove('hidden');
+    coupnArea.classList.add('hidden');
+ }
+
+ function applyMessege(){
+    if (usedCoupn.value === firstCoupn){
+        Swal.fire({
+            title: firstCoupn,
+            text: "Successfully applied Coupon!",
+            icon: "success"
+          });
+    }else if(usedCoupn.value === secondCoupn){
+        Swal.fire({
+            title: secondCoupn,
+            text: "Successfully applied Coupon!",
+            icon: "success"
+          });
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: "Wrong",
+            text: "Please enter the correct coupon",
+          });
+    }
+ }
+
+function applyCoupn(){
+    const dicount = document.getElementById('dicount')
+    const dicountPrice = document.getElementById('dicountPrice')
+    const totalPrice = document.getElementById('totalPrice');
+    const totalAmount = parseFloat(totalPrice.innerText);
+
+    
+    if (usedCoupn.value === firstCoupn){
+        const discountAmount = totalAmount * 0.15;
+        discountTotalAmount = totalAmount - discountAmount;
+        dicount.innerText= parseFloat( discountAmount );
+        dicountPrice.innerText= parseFloat(discountTotalAmount);
+        unhideArea();
+    }else if(usedCoupn.value === secondCoupn){
+        const discountAmount = totalAmount * 0.2;
+        discountTotalAmount = totalAmount - discountAmount;
+        dicount.innerText= parseFloat( discountAmount );
+        dicountPrice.innerText= parseFloat( discountTotalAmount );
+        unhideArea();
+    }else{
+        usedCoupn.value= '';
+        applyButton.classList.remove('btn');
+        applyButton.classList.remove('bg-[#1DD100]');
+        applyButton.classList.remove('text-white');
+    }
+}
+
+usedCoupn.addEventListener('keyup', function(){
+    if(selectedSeat.length === 4){
+        const count =buttonVisible(applyButton , usedCoupn);
+        if(count === 1){
+            applyButton.addEventListener('click', conditionOfCoupon);
+        }else{
+            applyButton.removeEventListener('click', conditionOfCoupon);
+        }
+    }
+});
---
+++ JS/select.js
@@ -0,0 +1,109 @@
+const seats = document.querySelectorAll(".seat");
+const seletedSeats= document.getElementById('addSeats');
+const removeText= document.getElementById('h1');
+const availableSet= document.getElementById('availableSet');
+const bookingSeat = document.getElementById('bookingSeat');
+const nextButton = document.getElementById('nextButton');
+const inputNumber= document.getElementById('inputNumber');
+const ticketPrice= document.getElementById('ticketPrice');
+
+var selectedSeat = [];
+
+
+function setTotalAmount(){
+    const dicountPrice = document.getElementById('dicountPrice');
+    const totalPrice= document.getElementById('totalPrice');
+    const total=parseFloat(totalPrice.innerText)+parseFloat(ticketPrice.innerText);
+    totalPrice.innerText = total;
+    dicountPrice.innerText = total;
+    applyCoupn();
+}
+
+function updateSeat(){
+    availableSet.innerText= (40 - selectedSeat.length) ;
+    bookingSeat.innerText= selectedSeat.length;
+    setTotalAmount();
+}
+
+
+
+function addStyle(seatId){
+    const seat = document.getElementById(seatId);
+    const addSeats = document.createElement('p');
+    seat.classList.add('bg-green-500');
+    seat.classList.add('text-white');
+    addSeats.classList.add('flex');
+    addSeats.classList.add('justify-between');
+    addSeats.innerHTML= "<span>"+seatId+"</span><span>Economy</span><span>550</span>";
+    seletedSeats.appendChild(addSeats);
+}
+
+function checkSelects(seatId){
+    if(!selectedSeat.includes(seatId)){
+        selectedSeat.push(seatId);
+        addStyle(seatId);
+        updateSeat();
+    }else{
+        Swal.fire({
+            text: "This seat is already selected!",
+          });
+    }    
+}
+
+
+for(let i=0; i<seats.length; i++){
+    const seat = seats[i];
+    seat.addEventListener('click',function(){
+        
+        if( selectedSeat.length === 0){
+            seletedSeats.removeChild(removeText);
+            checkSelects(seat.innerText);
+        }else if( selectedSeat.length < 4){
+            checkSelects(seat.innerText);      
+        }else{
+            Swal.fire({
+                text: "You select only 4 seats!",
+              });
+        }        
+    });
+    
+}
+
+
+function getTicket(){
+    if( isNaN(inputNumber.value) || inputNumber.value ===""){
+        Swal.fire({
+            text: "Please write correct Number!",
+          });
+    }else if(selectedSeat.length === 0){
+        Swal.fire({
+            text: "Please select atleast one seat!",
+          });
+    }else{
+        successMessage();
+    }
+}
+
+inputNumber.addEventListener('input', function(){
+    if(selectedSeat.length !== 0){
+        const count =buttonVisible(nextButton , inputNumber);
+        if(count === 1){
+            nextButton.addEventListener('click', getTicket);
+        }else{
+            nextButton.removeEventListener('click', getTicket);
+        }
+    }
+});
+
+function conditionOfCoupon(){
+    if(selectedSeat.length !== 0){
+        applyCoupn();
+        applyMessege();
+    }else{
+        Swal.fire({
+            title: "",
+            text: "Please Select Atlist one seat!",
+            icon: ""
+          });
+    }
+}
